c2136c8d5b35104ee2a8ff0b0938e7b63b77efe0
alekseypopov <aleksey.popov1983@gmail.com>
2012-02-25 10:31:02 +0200

    requirement change

--- nursery/logic/views.py
+++ nursery/logic/views.py
@@ -2,6 +2,3 @@ from django.http import HttpResponse
 
 def home(request):
     return HttpResponse("Nursery Management System")
-
-def test(request):
-    return HttpResponse("Testing")
--- nursery/testapi/views.py
+++ nursery/testapi/views.py
@@ -1,5 +1,7 @@
 from django.http import HttpResponse
+import urllib2, urllib, json
 
 def test_create_nursery(request):
+    host_url = "http://"
     return HttpResponse("test create nursery")
 
