a6975592b2dd50d076a7bf1fce6df51e6f7e9e74
alekseypopov <aleksey.popov1983@gmail.com>
2012-02-26 06:04:03 +0200

    addNewGreenhouse working

--- nursery/api/urls.py
+++ nursery/api/urls.py
@@ -1,11 +1,11 @@
 from django.conf.urls.defaults import *
 from piston.resource import Resource
-from handlers import CreateNursery_Handler
+from handlers import CreateNursery_Handler, CreateGreenhouse_Handler
 
 create_newNursery = Resource(handler = CreateNursery_Handler)
 create_newGreenhouse = Resource(handler = CreateGreenhouse_Handler)
 
 urlpatterns = patterns('',
-    url('^nursery/$', create_NewNursery),
+    url('^nursery/$', create_newNursery),
     url('^nursery/(?P<nursery_id>\d+)/addGreenhouse/$', create_newGreenhouse),
 )
